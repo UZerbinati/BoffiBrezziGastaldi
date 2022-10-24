59a9f9d0347636c92b6acd7533f328682b035395
UZ <umberto.zerbinati@oriel.ox.ac.uk>
2022-10-24 22:07:11 +0000

    Added Zero G Examples

--- test/ZeroG/Q1P0.py
+++ test/ZeroG/Q1P0.py
@@ -3,23 +3,22 @@ import numpy as np
 from firedrake.petsc import PETSc
 from slepc4py import SLEPc
 
+msh = SquareMesh(64,64,np.pi,quadrilateral=True)
 
-msh = UnitSquareMesh(5,5,diagonal="crossed")
-V = VectorFunctionSpace(msh, "CG", 4)
-Q = FunctionSpace(msh, "DG", 3)
-X = V*Q
-u,p = TrialFunctions(X)
-v,q = TestFunctions(X)
+S = VectorFunctionSpace(msh, "Q", 1)
+V = FunctionSpace(msh, "DG", 0)
+X = S*V
+s,u = TrialFunctions(X); t,v = TestFunctions(X)
 
-a = (inner(grad(u), grad(v)) - inner(p, div(v)) + inner(div(u), q))*dx
-m = inner(u,v)*dx
+a = (inner(s,t)+inner(div(t),u)+inner(div(s),v))*dx
+m = -inner(u,v)*dx
 
 print("Problem Set Up !")
 
 sol = Function(X)
 
-print("Assembling ...")
 bc = DirichletBC (X.sub(0), as_vector([0.0,0.0]) , [1,2,3,4]) # Boundary condition
+print("Assembling ...")
 A = assemble (a,bcs=bc)
 M = assemble (m)
 Asc, Msc = A.M.handle, M.M.handle
@@ -28,15 +27,13 @@ print("Solving ...");
 
 E = SLEPc.EPS().create()
 E.setType(SLEPc.EPS.Type.ARNOLDI)
-E.setProblemType(SLEPc.EPS.ProblemType.GHEP);
-E.setDimensions(5,SLEPc.DECIDE);
+E.setProblemType(SLEPc.EPS.ProblemType.GNHEP);
+E.setDimensions(25,SLEPc.DECIDE);
 E.setOperators(Asc,Msc)
 ST = E.getST();
 ST.setType(SLEPc.ST.Type.SINVERT)
-ST.setShift(12.0)
-KSP = ST.getKSP()
-KSP.setType("minres")
-PC = KSP.getPC();
+ST.setShift(1.0)
+PC = ST.getKSP().getPC();
 PC.setType("lu");
 PC.setFactorSolverType("mumps");
 E.setST(ST);
@@ -45,14 +42,12 @@ nconv = E.getConverged()
 print("Number of converged eigenvalues is: {}".format(nconv))
 for k in range(nconv): 
     vr, vi = Asc.getVecs()
-    if k == 0:
+    if k == 1:
         with sol.dat.vec_wo as vr:
             lam = E.getEigenpair(k, vr, vi)
-    u,p = sol.split()
-    u.rename("Vel")
-    p.rename("Prs")
-    File("SVish.pvd").write(u,p)
-    print("Svaed !")
+            u,p = sol.split()
+            u.rename("Vel")
+            p.rename("Prs")
+            File("Q1P0.pvd").write(u,p)
     lam = E.getEigenpair(k, vr, vi)
     print("[{}] Eigenvalue: {}".format(k,lam.real))
-
